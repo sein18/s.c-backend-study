66ada13a4d6b09109b49b0b24a4a02b61f9bc12f
sein <tpdls7080@naver.com>
2022-09-08 09:22:20 +0900

    jQuery로 변경하기

--- 박세인-9.7/Calendar/script.js
+++ 박세인-9.7/Calendar/script.js
@@ -1,61 +1,98 @@
  (function() {
     'use strict';
-
     const nowdate = new Date(); // 현재시간
-    const date = new Date(); // 현재시간
-    const prevdate = new Date(); 
-    const nextdate = new Date(); 
-    let weekName = ['일','월','화','수','목','금','토'];
-
-    
-    // console.log(date); 
-    // console.log(date.getFullYear()); //년도 
-    // console.log(date.getMonth() + 1); // 월 - getMonth()의 반환값이 0 ~ 11이기 때문에 +1을 해주어야 된다.
-    // console.log(date.getDate()); // 일
-    // console.log(date.getDay()); // 요일
-    // console.log(date.getHours()); // 시간
-    // console.log(date.getMinutes()); // 분
-    // console.log(date.getSeconds()); // 초
-    // console.log(date.getMilliseconds()); // 밀리초
-
-    // console.log(date.toLocaleDateString());
-    // console.log(date.toLocaleTimeString());
-    // console.log(date.toLocaleString());
-
-    document.querySelector("h1").innerText = `${ date.getFullYear()}년  ${date.getMonth() + 1}월`;
-    document.querySelector("P").innerText = `TODAY ${nowdate.toLocaleDateString()}`;
     
 
-    document.querySelector('i[class="fas fa-angle-left prev"]').addEventListener('click',function(event){
-        console.log(date.setMonth(date.getMonth()-1));
-        if(date.getMonth());
-        prevdate.setMonth(date.getMonth());
-        // nextdate = date. set
-    });
+    class CDate{
+        date = new Date(); // 현재시간
+        prevdate ; 
+        nextdate ; 
+
+        ck(){
+            
+        }
+
+        prev(){
+
+        }
+        
+        next(){
+
+        }
+    }
+    
+    // let weekName = ['일','월','화','수','목','금','토'];
+
+    
+    // // console.log(date); 
+    // // console.log(date.getFullYear()); //년도 
+    // // console.log(date.getMonth() + 1); // 월 - getMonth()의 반환값이 0 ~ 11이기 때문에 +1을 해주어야 된다.
+    // // console.log(date.getDate()); // 일
+    // // console.log(date.getDay()); // 요일
+    // // console.log(date.getHours()); // 시간
+    // // console.log(date.getMinutes()); // 분
+    // // console.log(date.getSeconds()); // 초
+    // // console.log(date.getMilliseconds()); // 밀리초
+
+    // // console.log(date.toLocaleDateString());
+    // // console.log(date.toLocaleTimeString());
+    // // console.log(date.toLocaleString());
+
+    // document.querySelector("h1").innerText = `${ date.getFullYear()}년  ${date.getMonth() + 1}월`;
+    // document.querySelector("P").innerText = `TODAY ${nowdate.toLocaleDateString()}`;
+    
+
+    // document.querySelector('i[class="fas fa-angle-left prev"]').addEventListener('click',function(event){
+    //     console.log(date.setMonth(date.getMonth()-1));
+    //     if(date.getMonth());
+    //     prevdate.setMonth(date.getMonth());
+    //     // nextdate = date. set
+    // });
     
     
-    document.querySelector('i[class="fas fa-angle-right next"]').addEventListener('click',function(event){
-        console.log(date.getMonth());
-        if(date.getMonth());
-        prevdate = date.setMonth(date.getMonth());
-        // nextdate = date. set
+    // document.querySelector('i[class="fas fa-angle-right next"]').addEventListener('click',function(event){
+    //     console.log(date.getMonth());
+    //     if(date.getMonth());
+    //     prevdate = date.setMonth(date.getMonth());
+    //     // nextdate = date. set
        
-    }); 
-    //여기부터 진행
-    // const divbtns = document.querySelectorAll('div[class=days] div') 
-    // divbtns
+    // }); 
+    // //여기부터 진행
+    // // const divbtns = document.querySelectorAll('div[class=days] div') 
+    // // divbtns
 
-    // const btns = document.querySelectorAll('.btn-item');
-	// btns.forEach(btn => {
-	// 	btn.addEventListener('click', () => {
-	// 		btn.classList.toggle('active');
-	// 	});
-	// });
+    // // const btns = document.querySelectorAll('.btn-item');
+	// // btns.forEach(btn => {
+	// // 	btn.addEventListener('click', () => {
+	// // 		btn.classList.toggle('active');
+	// // 	});
+	// // });
 
-     
+    
 
 })();
 
-function ck(date){
+class CDate{
+    date = new Date(); // 현재시간
+    prevdate ; 
+    nextdate ; 
+
+    ck(){
+        this.prevdate = new Date(this.date);
+        this.date =  new Date(this.date.setMonth(this.date.getMonth()-1));
+        this.nextdate = new Date(this.date.setMonth(this.date.getMonth()-1));
+        console.log(this.prevdate);
+        console.log(this.date);
+        console.log(this.nextdate);
+    }
+
+    prev(){
+
+    }
     
+    next(){
+
+    }
 }
+dt = new CDate();
+dt.ck();
---
+++ 박세인-9.8/jQuery변경/js/temmain.js
@@ -0,0 +1,192 @@
+(function() {
+    'use strict';
+    var arr = []; 
+    /** Validation Check Start **/
+    /* 이름 */
+    $('#username').blur(function() {
+        const value = this.value,
+              elParent = this.parentElement,
+              parentClassList = elParent.classList;
+        if(value) {
+            if(!/^[ㄱ-ㅎㅏ-ㅣ가-힣]+$/g.test(value)) {
+                parentClassList.add('error');
+                parentClassList.remove('success');
+            }else{ 
+                parentClassList.add('success');
+                parentClassList.remove('error');
+            }
+        }else{
+            parentClassList.remove('success', 'error');
+        }
+    });
+
+    /* 이메일 */
+    $('#email').blur(function() {
+        /*
+            (실습문제 1) 이메일 유효성검사 작성
+              - 이메일 유효성검사 정규식 : /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                * 다른 이메일 정규식을 검색해서 사용해도 됨
+                * 다른 유효성검사 항목 참고하여 작성 (이름, 비밀번호 등)
+
+        */
+        const value = this.value,
+              elParent = this.parentElement,
+              parentClassList = elParent.classList;
+
+        if(value) {
+            if(!/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)) {
+                parentClassList.add('error');
+                parentClassList.remove('success');
+            }else{
+                parentClassList.add('success');
+                parentClassList.remove('error');
+            }
+        }else{
+            parentClassList.remove('success', 'error');
+        }
+    });
+
+    /* 비밀번호 */
+    $('#pwd').blur(function() {
+        const value = this.value,
+              elParent = this.parentElement,
+              parentClassList = elParent.classList;
+
+        if(value) {
+            if(!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/g.test(value)) {
+                parentClassList.add('error');
+                parentClassList.remove('success');
+            }else{
+                parentClassList.add('success');
+                parentClassList.remove('error');
+            }
+        }else{
+            parentClassList.remove('success', 'error');
+        }
+    });
+
+    /* 비밀번호확인 */
+    $('#pwdConfirm').blur(function() {
+        const value = this.value,
+              pwdValue = document.getElementById('pwd').value,
+              elParent = this.parentElement,
+              parentClassList = elParent.classList;
+
+        if(value) {
+            if(value !== pwdValue) {
+                parentClassList.add('error');
+                parentClassList.remove('success');
+            }else{
+                parentClassList.add('success');
+                parentClassList.remove('error');
+            }
+        }else{
+            parentClassList.remove('success', 'error');
+        }
+    });
+    
+    /** Validation Check End **/
+
+    
+    // 회원가입 submit
+    $('#form').submit(function(event) {
+        //필수 값이 없으면 0 있으면 1 
+        let yesno = 1;
+        
+        const ck = [$('#username'),$('#email'),$('#pwd'),$('#privacy')]
+        for(let element of ck) {
+            if(!element.value || (element.type === 'checkbox' && !element.checked)) {
+                alert(`${document.querySelector(`label[for="${element.id}"]`).text()}가(이) 입력되지 않았습니다.`);
+                event.preventDefault();
+                break;
+            }
+        }
+
+        if(yesno == 1){
+            let Jarr = JSON.parse(localStorage.getItem('arr'));
+            //로컬 스토리즈가 클리어 상태이면 x
+            console.log(Jarr);
+            if(Jarr && Jarr.length!=0){
+                arr = Jarr; 
+            }
+
+            const username = document.getElementById('username').value;
+            const email = document.getElementById('email').value;
+            const pwd = document.getElementById('pwd').value;
+            const userAddr = document.getElementById('userAddr').value;
+            let gender = document.querySelectorAll('input[name="gender"]:checked')[0].value;
+            const privacy = document.getElementById('privacy').checked?'동의':'비동의';
+            const marketing = document.getElementById('marketing').checked?'동의':'비동의';
+            
+            arr.push({username:username,
+                      email:email,
+                      pwd:pwd,
+                      userAddr:userAddr,
+                      gender:gender,
+                      privacy:privacy,
+                      marketing:marketing});
+             
+            //로컬스토리즈에 등록
+            localStorage.setItem('arr',  JSON.stringify(arr));
+        }
+        
+    });
+
+    window.onload = function lod(){ 
+        let Jarr = JSON.parse(localStorage.getItem('arr'));
+        if(Jarr){
+            for(let i =0;i<Jarr.length;i++){
+                // 기존 HTML tbody 와 템플릿 열로 테이블을 인스턴스화합니다
+                let t = document.querySelector('#user_template');
+                // console.log(t.content);
+                // console.log(t);
+                // 템플릿을 복사 (true 자식을 가져오고, false자식을 안 가져옴)
+                var clone = document.importNode(t.content, true);
+                let td = clone.querySelectorAll("td");
+                // console.log(td);
+                
+                td[1].innerText = Jarr[i].username;
+                td[2].innerText = Jarr[i].email;
+                td[3].innerText = Jarr[i].pwd;
+                td[4].innerText = Jarr[i].userAddr;
+                td[5].innerText = Jarr[i].gender;
+                td[6].innerText = Jarr[i].privacy;
+                td[7].innerText = Jarr[i].marketing;
+
+                let tb = document.querySelector("tbody"); 
+                tb.appendChild(clone);
+            }
+        }else{
+            console.log("오류");
+        }
+    };
+    
+    //전체 체크 클릭
+    $('#check_all').click(function(event) {
+        document.querySelectorAll('input[class="check"]').forEach((el) => {
+            el.checked = event.target.checked;
+        });
+    });
+
+    //삭제 event
+    $('#delete').click(function() {
+        const getcks = document.querySelectorAll(`input[class="check"]`);
+        let Jarr = JSON.parse(localStorage.getItem('arr'));
+
+        //배열 순번이 꼬이지 않도록 뒤에서 삭제
+        for(let i = getcks.length-1;i>= 0;i--){
+            if(getcks[i].checked==true){
+                Jarr.splice(i,1); 
+                console.log(Jarr);
+                localStorage.setItem('arr', JSON.stringify(Jarr));
+                $(getcks[i]).parent().parent().remove();
+            }
+        }
+    });
+
+})();
+
+   //부분 체크 클릭
+   function selectCheckBox(){ 
+        document.getElementById('check_all').checked = document.querySelectorAll(`input[class="check"]:checked`).length == document.querySelectorAll(`input[class="check"]`).length;
+}
